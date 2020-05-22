0c6b27a525b56b9c8320ab01c4b3b1cb2d71bd28
AnastasiaTuryk <62389381+AnastasiaTuryk@users.noreply.github.com>
2020-05-22 14:54:27 +0300

    Update task 1.cpp

--- CW_3/task1/task 1.cpp
+++ CW_3/task1/task 1.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 struct Book
 {
-	char NameBook[100];
-	char Name[100];
-	int year;
-	int genre;
+    char NameBook[100];
+    char Name[100];
+    int year;
+    int genre;
 };
 
 void sorted(Book b[10], int len) {
@@ -17,12 +17,12 @@ void sorted(Book b[10], int len) {
 
     for (int i = 0; i < len; ++i) {
         for (int j = i; j < len; ++j) {
-            if (b[i].year== b[j].year && b[i].genre > b[j].genre) {
+            if (b[i].year == b[j].year && b[i].genre > b[j].genre) {
                 Book temp = b[i];
                 b[i] = b[j];
                 b[j] = temp;
             }
-            else if (b[i].year> b[j].year) {
+            else if (b[i].year > b[j].year) {
                 Book temp = b[i];
                 b[i] = b[j];
                 b[j] = temp;
@@ -30,7 +30,7 @@ void sorted(Book b[10], int len) {
         }
     }
     for (int i = 0; i < len; i++) {
-        file << b[i].NameBook << "," << b[i].Name<< "," << b[i].year << ", " << b[i].genre << endl;
+        file << b[i].NameBook << "," << b[i].Name << "," << b[i].year << ", " << b[i].genre << endl;
 
     }
     file.close();
@@ -44,77 +44,50 @@ int main()
     ifstream fin("in.txt");
     int size = 0;
     char s[100];
-    
-
-    try {
-        if (!fin)
-        {
-            cout << "your file is not open!";
-        }
-
-        while (fin.getline(s, 100))
-        {
-            char* ch;
-            ch = strtok(s, " ");
-            int j = 1;
-            while (ch != NULL)
-            {
-                switch (j)
-                {
-                case 1:
-                    strcpy_s(b[size].NameBook, ch);
-                    j++;
-                    break;
-                case 2:
-                    strcpy_s(b[size].Name, ch);
-                    j++;
-                    break;
-                case 3:
-                    b[size].year = atoi(ch);
-                    j++;
-                    break;
-                case 4:
-                    b[size].genre = atoi(ch);
-                    j = 1;
-                    break;
-                default:
-                    break;
-                }
-                ch = strtok(NULL, " ");
-            }
-
-            size++;
-
-        }
-
-
-        int min = b[0].year;
-        int ind = 0;
-        for (int i = 1; i < size; i++) {
-            if (b[i].year < min)
-            {
-                min = b[i].year;
-                ind = i;
-            }
-        }
-
-
-        cout << "Older : \n";
-        cout << b[ind].NameBook << ", " << b[ind].Name << ", " << b[ind].year << ", " << b[ind].genre << endl;
-        cout << "UnSorted: \n";
-        for (int i = 0; i < size; i++) {
-            cout << b[i].NameBook << ", " << b[i].Name<< ", " << b[i]. year<< ", " << b[i].genre<< endl;
-
-        }
-        sorted(b, size);//ñîðòóâàííÿ êíèæîê çà íàçâîþ  ³ æàíðîì
-    }
-
-    catch (const char* exception)
+    if (fin.is_open())
     {
-        std::cerr << "Error: " << exception << '\n';
+        while (!fin.eof())
+        {
+            fin >> b[size].NameBook;
+            fin >> b[size].Name;
+            fin >> b[size].year;
+            fin >> b[size].genre;
+            size++;
+        }
+    }
+    fin.close();
+
+    int max = b[0].year;
+    int ind = 0;
+    for (int i = 1; i < size; i++) 
+    {
+        if (b[i].year < max)
+        {
+            max = b[i].year;
+            ind = i;
+        }
     }
 
+    cout << "Older : \n";
+    cout << b[ind].NameBook << ", " << b[ind].Name << ", " << b[ind].year << ", " << b[ind].genre << endl;
+    cout << "UnSorted: \n";
+    for (int i = 0; i < size; i++) 
+    {
+        cout << b[i].NameBook << ", " << b[i].Name << ", " << b[i].year << ", " << b[i].genre << endl;
 
+    }
+    sorted(b, size);//Ã±Ã®Ã°Ã²Ã³Ã¢Ã Ã­Ã­Ã¿ ÃªÃ­Ã¨Ã¦Ã®Ãª Ã§Ã  Ã­Ã Ã§Ã¢Ã®Ã¾  Â³ Ã¦Ã Ã­Ã°Ã®Ã¬
     system("pause");
     return 0;
+
 }
+
+       
+
+
+ 
+            
+
+           
+
+ 
